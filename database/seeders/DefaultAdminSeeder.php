ad375552aa450f0f44bb5abbf593acc11383263e
Francisco Javier Rodríguez Hernández <fj_rh@hotmail.com>
2024-06-29 10:42:37 -0400

    Tests unitarios mejorados para los endpoints de usuario

--- database/seeders/DefaultAdminSeeder.php
+++ database/seeders/DefaultAdminSeeder.php
@@ -13,36 +13,34 @@ class DefaultAdminSeeder extends Seeder
      * Run the database seeds.
      */
 
-     //ESTOS SEEDERS SON NECESARIOS PARA LAS PRUEBAS UNITARIAS, SI SE ELIMINAN NO SE EJECUTARAN, POR LO TANTO LOS DATOS DE LA BASE DE DATOS SON DE PRUEBA.
+    //ESTOS SEEDERS SON NECESARIOS PARA LAS PRUEBAS UNITARIAS, SI SE ELIMINAN NO SE EJECUTARAN, POR LO TANTO LOS DATOS DE LA BASE DE DATOS SON DE PRUEBA.
     public function run(): void
     {
-        if (User::count() == 0) {
-            try {
-                User::create([
-                    'id' => Str::uuid(),
-                    'user' => TestCase::defaultAdmin,
-                    'password' => TestCase::defaultPassword,
-                    'level' => '1',
-                ]);
-                info('Se ha creado el administrador por defecto: defaultAdmin');
 
-                User::create([
-                    'id' => Str::uuid(),
-                    'user' => TestCase::defaultUser,
-                    'password' => TestCase::defaultPassword,
-                    'level' => '2',
-                ]);
+        try {
+            User::create([
+                'id' => Str::uuid(),
+                'user' => TestCase::defaultAdmin,
+                'password' => TestCase::defaultPassword,
+                'level' => '1',
+            ]);
+            info('Se ha creado el administrador por defecto: defaultAdmin');
 
-                User::create([
-                    'id' => Str::uuid(),
-                    'user' => TestCase::defaultGuest,
-                    'password' => TestCase::defaultPassword,
-                    'level' => '3',
-                ]);
+            User::create([
+                'id' => Str::uuid(),
+                'user' => TestCase::defaultUser,
+                'password' => TestCase::defaultPassword,
+                'level' => '2',
+            ]);
 
-            } catch (\Throwable $th) {
-                info($th->getMessage());
-            }
+            User::create([
+                'id' => Str::uuid(),
+                'user' => TestCase::defaultGuest,
+                'password' => TestCase::defaultPassword,
+                'level' => '3',
+            ]);
+        } catch (\Throwable $th) {
+            info($th->getMessage());
         }
     }
 }
